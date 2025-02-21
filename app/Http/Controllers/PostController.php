17b43b954f258a3e21c8465409c9f04536f98f20
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-02-21 20:57:02 +0100

    feat(controller): apply 'admin' middleware to PostController for admin actions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+        $this->middleware('admin')->only(['create', 'store', 'edit', 'update', 'destroy']);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title'   => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        $validated['user_id'] = Auth::id();
+        Post::create($validated);
+
+        return redirect()->route('posts.index')->with('success', 'Post został dodany!');
+    }
 }
