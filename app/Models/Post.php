2baa2499a69d575917fc4258b3a0647075c3b67c
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-02-21 21:29:48 +0100

    feat(models): define relationships between Post and Comment models (one-to-many)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'image',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
