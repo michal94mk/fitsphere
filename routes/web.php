e8ff977695f3bb6b2afb7e3098190c02098b351e
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-02-21 20:31:11 +0100

    feat(middleware): create Admin middleware to restrict admin-only routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,5 +20,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::resource('posts', PostController::class);
+});
+
 
 require __DIR__.'/auth.php';
