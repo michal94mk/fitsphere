166518600c7695601023c772d2ad9863070f5d28
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-05-08 02:24:35 +0200

    fix: update experience translation to lowercase in Polish language file

--- resources/views/livewire/admin/trainers-show.blade.php
+++ resources/views/livewire/admin/trainers-show.blade.php
@@ -89,7 +89,7 @@
                 </div>
                 @if(isset($trainer->experience))
                 <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-500">Doświadczenie (lata)</dt>
+                    <dt class="text-sm font-medium text-gray-500">doświadczenie (lata)</dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                         {{ $trainer->experience ?? '0' }}
                     </dd>
